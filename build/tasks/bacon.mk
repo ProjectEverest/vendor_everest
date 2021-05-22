1df1828d9bbe72f5578bb08ed5a9de40ef69115c
OdSazib <odsazib@gmail.com>
2021-05-22 09:53:36 +0600

    vendor: tools: Refactor json script for Everest OTA
    * Also print output according to Everest OTA
    
    Change-Id: I533cb75a18bd5cfd3d84b112ba5a30c243e0ea98

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -33,6 +33,6 @@ bacon: $(DEFAULT_GOAL) $(INTERNAL_OTA_PACKAGE_TARGET)
 	echo -e ${CL_BLD}${CL_GRN}"Zip: "${CL_RED} $(EVEREST_TARGET_PACKAGE)${CL_RST}
 	echo -e ${CL_BLD}${CL_GRN}"SHA256: "${CL_RED}" `cat $(EVEREST_TARGET_PACKAGE).sha256sum | awk '{print $$1}' `"${CL_RST}
 	echo -e ${CL_BLD}${CL_GRN}"Size:"${CL_RED}" `du -sh $(EVEREST_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
-	echo -e ${CL_BLD}${CL_GRN}"TimeStamp:"${CL_RED}" `cat $(PRODUCT_OUT)/system/build.prop | grep ro.build.date.utc | cut -d'=' -f2 | awk '{print $$1}' `"${CL_RST}
-	echo -e ${CL_BLD}${CL_GRN}"Integer Value:"${CL_RED}" `wc -c $(EVEREST_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_GRN}"ID:"${CL_RED}" `md5sum $(EVEREST_TARGET_PACKAGE) | cut -d ' ' -f 1`"${CL_RST}
+	echo -e ${CL_BLD}${CL_GRN}"Path:"${CL_RED}" $(EVEREST_TARGET_PACKAGE)"${CL_RST}
 	echo -e ${CL_BLD}${CL_RED}"================================================================================"${CL_RED}
