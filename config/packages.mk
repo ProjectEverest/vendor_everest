c4353496d68225c57caad737ca55d382814d3b9c
Oliver Scott <olivercscott@gmail.com>
2022-10-11 20:39:01 +0200

    config: Build BatteryStatsViewer
    Change-Id: I1f712533f1914f7155f7a6657e3fa2095b4ea83a

--- config/packages.mk
+++ config/packages.mk
@@ -103,6 +103,10 @@ PRODUCT_PACKAGES += \
     NetworkStackOverlay \
     ThemedIconsOverlay
 
+# BatteryStatsViewer
+PRODUCT_PACKAGES += \
+    BatteryStatsViewer
+
 # BtHelper
 PRODUCT_PACKAGES += \
     BtHelper
