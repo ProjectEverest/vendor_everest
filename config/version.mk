20845a1d651e76bd1ec66b98cd3c1ad4d181ebc7
Ido Ben-Hur <idoybh2@gmail.com>
2023-03-02 21:57:53 +0200

    config: Conditionally enable dev keys signing

--- config/version.mk
+++ config/version.mk
@@ -23,3 +23,17 @@ LINEAGE_VERSION := EverestOS-$(EVEREST_VERSION)-$(EVEREST_CODENAME)-$(LINEAGE_BU
 
 # Display version
 LINEAGE_DISPLAY_VERSION := EverestOS-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(LINEAGE_BUILD)-V$(EVEREST_VERSION)
+
+# Signing
+ifeq (user,$(TARGET_BUILD_VARIANT))
+ifneq (,$(wildcard .android-certs/releasekey.pk8))
+PRODUCT_DEFAULT_DEV_CERTIFICATE := .android-certs/releasekey
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.oem_unlock_supported=1
+endif
+ifneq (,$(wildcard .android-certs/verity.pk8))
+PRODUCT_VERITY_SIGNING_KEY := .android-certs/verity
+endif
+ifneq (,$(wildcard .android-certs/otakey.x509.pem))
+PRODUCT_OTA_PUBLIC_KEYS := .android-certs/otakey.x509.pem
+endif
+endif
