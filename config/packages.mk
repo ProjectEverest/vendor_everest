c5104868e980e7774b95b7d438d26222529c989b
ponces <ponces26@gmail.com>
2023-11-15 16:02:38 +0000

    vendor: Add AOSPA's face unlock implementation
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/packages.mk
+++ config/packages.mk
@@ -16,6 +16,16 @@
 PRODUCT_PACKAGES += \
     EverestWallpaperStub
 
+# Face Unlock
+PRODUCT_PACKAGES += \
+    FaceUnlock
+
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.face.sense_service=true
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+
 # Extra tools in Everest
 PRODUCT_PACKAGES += \
     bash \
