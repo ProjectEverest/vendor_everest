08b46d73dba3f3e98f6db135b5336d23bc99b8ee
Mohammad Kibria <106745675+kibria5@users.noreply.github.com>
2024-05-09 08:52:19 +0000

    [SQUASH] config: Remove Android Go configurations
    *It's Causing Slowdown Some Device
    
    Revert "superior: Inherit several Android Go configurations"
    
    This reverts commit d4bd48d994ed1e8192aba4d95a3946bd6a2d903a.
    
    Revert "superior: Speed profile services and wifi-service to reduce RAM and storage"
    
    This reverts commit 55838fcd6f943665c8699a05e8b246b0d63bae90.

--- config/everest_props.mk
+++ config/everest_props.mk
@@ -70,10 +70,8 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     SystemUI
 
-# Speed profile services and wifi-service to reduce RAM and storage
-PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
-PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    dalvik.vm.systemuicompilerfilter=speed
 
 # Tethering - allow without requiring a provisioning app
 # (for devices that check this)
