e3c60dc8494583eb77d86c201e4184e3515c8fee
janak <niraulajanak2019@gmail.com>
2024-06-22 08:30:38 +0545

    vendor: Import our Bootanimation
    Special thanks to:
    @SudipRajbanshi - for logo
    @ztrahmet - for animation
    @XelXen - for fixes

---
+++ config/bootanimation.mk
@@ -0,0 +1,15 @@
+# Bootanimation
+ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
+     PRODUCT_COPY_FILES += vendor/lineage/bootanimation/bootanimation-1080p.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
+     PRODUCT_COPY_FILES += vendor/lineage/bootanimation/bootanimation-1440p.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
+     PRODUCT_COPY_FILES += vendor/lineage/bootanimation/bootanimation-720p.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+else
+    ifeq ($(TARGET_BOOT_ANIMATION_RES),)
+        $(warning "TARGET_BOOT_ANIMATION_RES is undefined, assuming 1080p")
+    else
+        $(warning "Current bootanimation res is not supported, forcing 1080p")
+    endif
+    PRODUCT_COPY_FILES += vendor/lineage/bootanimation/bootanimation-1080p.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+endif
--- config/common.mk
+++ config/common.mk
@@ -269,6 +269,9 @@ include vendor/lineage/config/version.mk
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 
+# Bootanimation
+include vendor/lineage/config/bootanimation.mk
+
 # EverestOS
 $(call inherit-product, vendor/lineage/config/everest.mk)
 include packages/overlays/Themes/themes.mk
