pixelstar-los/vendor_lineage

lineage: Initial EverestOS rebrand
Co-authored-by: Joey Huab <joey@evolution-x.org>
Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
Co-authored-by: Hemant Sachdeva <hemant.evolver@gmail.com>
Co-authored-by: Mudit200408 <muditpurohit2004.pm@gmail.com>
Signed-off-by: AnierinB <anierin@evolution-x.org>

prebuilt/common/bin/backuptool.sh
@@ -6,7 +6,7 @@
 export C=/tmp/backupdir
 export SYSDEV="$(readlink -nf "$2")"
 export SYSFS="$3"
-export V=21.0
+export V=14.0
 
 export ADDOND_VERSION=3
 
@@ -47,7 +47,7 @@ if [ ! -r $S/build.prop ]; then
   echo "Backup/restore is not possible. Partition is probably empty"
   return 1
 fi
-if ! grep -q "^ro.lineage.version=$V.*" $S/build.prop; then
+if ! grep -q "^ro.pixelstar.version=$V.*" $S/build.prop; then
   echo "Backup/restore is not possible. Incompatible ROM version: $V"
   return 2
 fi

prebuilt/common/bin/backuptool_ab.sh
@@ -5,7 +5,7 @@
 
 export S=/system
 export C=/postinstall/tmp/backupdir
-export V=21.0
+export V=14.0
 
 export ADDOND_VERSION=3
 
@@ -56,7 +56,7 @@ if [ ! -r /system/build.prop ]; then
   echo "Backup/restore is not possible. Partition is probably empty"
   return 1
 fi
-if ! grep -q "^ro.lineage.version=$V.*" /system/build.prop; then
+if ! grep -q "^ro.pixelstar.version=$V.*" /system/build.prop; then
   echo "Backup/restore is not possible. Incompatible ROM version: $V"
   return 2
 fi

ProjectEverest/vendor_everest

Co-authored-by: Janak Niroula <niraulajanak2019@gmail.com>

prebuilt/common/bin/backuptool.sh
@@ -6,7 +6,7 @@
 export C=/tmp/backupdir
 export SYSDEV="$(readlink -nf "$2")"
 export SYSFS="$3"
-export V=21.0
+export V=14.0
 
 export ADDOND_VERSION=3
 
@@ -47,7 +47,7 @@ if [ ! -r $S/build.prop ]; then
   echo "Backup/restore is not possible. Partition is probably empty"
   return 1
 fi
-if ! grep -q "^ro.lineage.version=$V.*" $S/build.prop; then
+if ! grep -q "^ro.everest.version=$V.*" $S/build.prop; then
   echo "Backup/restore is not possible. Incompatible ROM version: $V"
   return 2
 fi

prebuilt/common/bin/backuptool_ab.sh
@@ -5,7 +5,7 @@
 
 export S=/system
 export C=/postinstall/tmp/backupdir
-export V=21.0
+export V=14.0
 
 export ADDOND_VERSION=3
 
@@ -56,7 +56,7 @@ if [ ! -r /system/build.prop ]; then
   echo "Backup/restore is not possible. Partition is probably empty"
   return 1
 fi
-if ! grep -q "^ro.lineage.version=$V.*" /system/build.prop; then
+if ! grep -q "^ro.everest.version=$V.*" /system/build.prop; then
   echo "Backup/restore is not possible. Incompatible ROM version: $V"
   return 2
 fi